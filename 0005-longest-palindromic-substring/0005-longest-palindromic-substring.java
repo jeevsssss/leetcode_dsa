2e1a83afd86e6709564216e28075e178ff7de21a
jeevaa <jeevaa.exe@gmail.com>
2023-06-01 20:54:39 +0530

    Time: 23 ms (68.83%), Space: 43.6 MB (27.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,32 +1,45 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int currLen = 0, maxLen = 0;
         int n = s.length();
-        if (n == 0)
-            return "";
-        if (n == 1)
+        String longestPalindrome = "";
+        
+        if(s.length() <= 1){
             return s;
-        
-        int minstart = 0, maxlen = 0;
-        
-        for (int i = 0; i < n; ) {
-            if (n - i < maxlen / 2)
-                break;
-            int l = i, r = i;
-            while (r < n - 1 && s.charAt(r) == s.charAt(r + 1))
-                r++;
-            i = r + 1;
-            while (l > 0 && r < n - 1 && s.charAt(l - 1) == s.charAt(r + 1)) {
-                l--;
-                r++;
+        }
+        //Code for odd length palindrome
+        for(int centerIndex = 1; centerIndex < n - 1; centerIndex++){
+            int leftIndex = centerIndex - 1, rightIndex = centerIndex + 1;
+            //Continue while both the indices are in range and  characters are matching
+            while(leftIndex >= 0 && rightIndex < n && s.charAt(leftIndex) == s.charAt(rightIndex)){
+                leftIndex--;
+                rightIndex++;
             }
-            
-            int newlen = r - l + 1;
-            if (newlen > maxlen) {
-                maxlen = newlen;
-                minstart = l;
+            //Calculate the current Palindrome length using formula (r - l + 1) 
+            currLen = (rightIndex - 1) - (leftIndex + 1) + 1 ;
+            //if curr length is greater than the maxLen we have, update longestPalindrome and maxLen
+            if(currLen > maxLen){
+                maxLen = currLen; 
+                longestPalindrome = s.substring(leftIndex + 1, rightIndex);
             }
         }
-        
-        return s.substring(minstart, minstart + maxlen);
+        //Code for even length palindrome
+        for(int centerIndex = 0; centerIndex < n ; centerIndex++){
+            int leftIndex = centerIndex, rightIndex = centerIndex + 1;
+            while(leftIndex >= 0 && rightIndex < n && s.charAt(leftIndex) == s.charAt(rightIndex)){
+                leftIndex--;
+                rightIndex++;
+            }
+            currLen = (rightIndex - 1) - (leftIndex + 1) + 1 ;
+            if(currLen > maxLen){
+                maxLen = currLen; 
+                longestPalindrome = s.substring(leftIndex + 1, rightIndex);
+            }   
+        }
+        //If you didn't find any palindrome then return 1 length palidnrome
+        if(longestPalindrome.isEmpty()){
+            return s.substring(0,1);
+        }
+        return longestPalindrome;
     }
 }
