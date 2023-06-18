c0fa57129e6fa23f053caad9a4f40e26b2f8fad8
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-18 13:43:08 +0530

    Time: 7 ms (79.39%), Space: 47 MB (46.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,16 +1,28 @@
-class Solution
-{
-public List<List<String>> groupAnagrams(String[] strs)
-{
-	Map<String, List<String>> map = new HashMap<>();
-	for(String str : strs) 
-    {
-		char[] chars = str.toCharArray();
-		Arrays.sort(chars);
-		String sortedStr = new String(chars);
-		map.putIfAbsent(sortedStr, new ArrayList<>());
-		map.get(sortedStr).add(str);
-	}
-	return new ArrayList<>(map.values());
- }
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Create a HashMap to store the anagram groups
+        Map<String, List<String>> map = new HashMap<>();
+        
+        // Iterate through each string in the input array
+        for (String str : strs) {
+            // Convert the string to a character array
+            char[] chars = str.toCharArray();
+            
+            // Sort the characters in ascending order
+            Arrays.sort(chars);
+            
+            // Convert the sorted character array back to a string
+            String sortedStr = new String(chars);
+            
+            // Check if the sorted string exists as a key in the HashMap
+            // If not, add it as a new key with an empty ArrayList as the value
+            map.putIfAbsent(sortedStr, new ArrayList<>());
+            
+            // Add the original string to the corresponding anagram group
+            map.get(sortedStr).add(str);
+        }
+        
+        // Return a list containing all the anagram groups
+        return new ArrayList<>(map.values());
+    }
 }
