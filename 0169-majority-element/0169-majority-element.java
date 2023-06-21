e7b2abe87d9a828899d92d7638cebd76cb85e642
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-21 23:36:45 +0530

    Time: 20 ms (14.07%), Space: 47.2 MB (58.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,5 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // high time and space complexity
-        // Arrays.sort(nums);
-        // return nums[nums.length/2];
         int n=nums.length;
         int result=0;
         HashMap<Integer,Integer>set=new HashMap<Integer,Integer>();
