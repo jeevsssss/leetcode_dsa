d366408b4afb4d6cb9700d8729f7390fb1636ff1
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-22 13:18:45 +0530

    Time: 2 ms (79.68%), Space: 44.7 MB (22.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,11 +2,10 @@ class Solution
 {
     public boolean canJump(int[] nums) 
     {
-        int n=nums.length;
         int max=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.length;i++)
         {
-            if(max<i)
+            if(i>max)
             {
                 return false;
             }
