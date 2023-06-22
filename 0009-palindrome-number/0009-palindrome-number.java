eb5db7bba04c507d06541a0ffc80cb039d61bede
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-22 16:03:54 +0530

    Time: 9 ms (99.46%), Space: 43.2 MB (18.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,33 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         String s = String.valueOf(x);  
+//         int i = 0;                     
+//         int j = s.length() - 1;        
+//         while(i <= j)                 
+//         {
+//             if(s.charAt(i) != s.charAt(j))
+//             {
+//                 return false;
+//             }
+//             i++;                              
+//             j--;                               
+//         }
+//         return true;
+        
+//     }
+// }
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);  
-        int i = 0;                     
-        int j = s.length() - 1;        
-        while(i <= j)                 
+        if (x<0 || (x!=0 && x%10==0))
         {
-            if(s.charAt(i) != s.charAt(j))
-            {
-                return false;
-            }
-            i++;                              
-            j--;                               
+            return false;
         }
-        return true;
-        
+        int rev = 0;
+        while (x>rev)
+        {
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return (x==rev || x==rev/10);
     }
 }
