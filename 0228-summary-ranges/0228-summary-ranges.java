0916a374765433279811525d8dcbcf1dcd914425
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-23 18:55:22 +0530

    Time: 5 ms (86.39%), Space: 40.9 MB (68.45%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,17 +1,23 @@
 class Solution {
-    public List<String> summaryRanges(int[] nums) {
-       ArrayList<String> al=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            int start=nums[i];
+    public List<String> summaryRanges(int[] nums)
+    {
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int temp=nums[i];
             while(i<nums.length-1 && nums[i]==nums[i+1]-1)
+            {
                 i++;
-            if(start!=nums[i]){
-                al.add(start+"->"+nums[i]);
             }
-            else{
-                al.add(""+start);
+            if(temp!=nums[i])
+            {
+                list.add(temp+"->"+nums[i]);
+            }
+            else
+            {
+                list.add(""+temp);
             }
         }
-        return al;
+        return list;
     }
 }
