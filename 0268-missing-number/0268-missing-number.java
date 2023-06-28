70d49ab670c7dcb1679ae2295a806c84fa510948
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-28 23:55:00 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (31.65%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,18 +1,31 @@
-class Solution {
-    public int missingNumber(int[] nums) {
-        HashSet<Integer>st=new HashSet<>();
-        int ans=0;
-        for(int num:nums)
-        {
-            st.add(num);
-        }
-        for(int i=0;i<=nums.length;i++)
-        {
-            if(!st.contains(i))
-            {
-                ans=i;
-            }
-        }
-        return ans;
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         HashSet<Integer>st=new HashSet<>();
+//         int ans=0;
+//         for(int num:nums)
+//         {
+//             st.add(num);
+//         }
+//         for(int i=0;i<=nums.length;i++)
+//         {
+//             if(!st.contains(i))
+//             {
+//                 ans=i;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+class Solution
+{
+    public int missingNumber(int[] nums)
+    {
+        int n = nums.length;
+        int expectedsum = (n*(n+1))/2; 
+        int currentsum=0;
+        for(int i=0;i<n;i++)
+            currentsum+=nums[i];
+
+        return expectedsum-currentsum;
     }
 }
