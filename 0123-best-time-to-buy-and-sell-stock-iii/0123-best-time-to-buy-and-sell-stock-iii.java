48f58d65a26597a944c0ff30d0ca89ae5c740ca4
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-30 16:14:23 +0530

    Time: 4 ms (94.15%), Space: 55.8 MB (88.07%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -4,7 +4,8 @@ class Solution {
             int buy1 = Integer.MAX_VALUE;
             int buy2 = Integer.MAX_VALUE;
             int sell1 = 0, sell2 = 0;
-            for (int i = 0; i < prices.length; i++) {
+            for (int i = 0; i < prices.length; i++)
+            {
                 buy1 = Math.min(buy1, prices[i]);
                 sell1 = Math.max(sell1, prices[i] - buy1);
                 buy2 = Math.min(buy2, prices[i] - sell1);
