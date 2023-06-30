35a7b87cc944ef2a6dbce2a3ecf400705104f1d6
Jeevaa S V <jeevaa.exe@gmail.com>
2023-06-30 16:28:42 +0530

    Time: 78 ms (45.63%), Space: 43 MB (69.22%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int lengthOfLIS(int[] nums)
+    {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int maxLength = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        
+        return maxLength;
+    }
+}
