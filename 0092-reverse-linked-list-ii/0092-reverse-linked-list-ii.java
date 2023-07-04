e3fc0a4136547835f9bbf59f3e0cc2630acaba0c
Jeevaa S V <jeevaa.exe@gmail.com>
2023-07-04 21:31:39 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (96.36%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -8,33 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        //moving to start of the sublist 
-        ListNode cur1 = dummy;
-        ListNode pre1 = null;
-        for(int i=0;i<m;i++){
-            pre1 = cur1;
-            cur1 = cur1.next;
+    public ListNode reverseBetween(ListNode head, int m, int n)
+    {
+        ListNode dummy=new ListNode(0);
+        ListNode current=dummy;
+        dummy.next=head;
+        ListNode prev=null;
+        for(int i=0;i<m;i++)
+        {
+            prev=current;
+            current=current.next;
         }
-        
-        //reversing it
-        ListNode cur2 = cur1;
-        ListNode pre2 = pre1;
-        ListNode q2;
-        for(int i=m;i<=n;i++){
-            q2 = cur2.next;
-            cur2.next = pre2;
-            pre2 = cur2;
-            cur2 = q2;
+        ListNode prev2=prev;
+        ListNode current2=current;
+        ListNode temp;
+        for(int i=m;i<=n;i++)
+        {
+            temp=current2.next;
+            current2.next=prev2;
+            prev2=current2;
+            current2=temp;
         }
-        
-        //connectng it
-        pre1.next = pre2;
-        cur1.next = cur2;
-        
+        prev.next=prev2;
+        current.next=current2;
         return dummy.next;
+        
     }
 }
+
