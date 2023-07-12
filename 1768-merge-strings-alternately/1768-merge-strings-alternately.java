34466687fbe58dc16d42cb5c3f6d1a3539ca920a
Jeevaa S V <jeevaa.exe@gmail.com>
2023-07-12 17:05:16 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (97.99%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,21 +1,21 @@
-class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        String res="";
-        int a=word1.length();
-        int b=word2.length();
-        int n=Math.max(a,b);
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public String mergeAlternately(String word1, String word2)
+    {
+        int w1Length = word1.length();
+        int w2Length = word2.length();
+        StringBuilder result = new StringBuilder();
+        int index=0;
+        
+        while(index<w1Length && index<w2Length)
         {
-            if(i<a)
-            {
-                res=res+word1.charAt(i);
-            }
-            if(i<b)
-            {
-                res=res+word2.charAt(i);
-            }
-            
+            result.append(word1.charAt(index));
+            result.append(word2.charAt(index));
+            index++;
         }
-        return res;
+        
+        result.append(word1.substring(index, w1Length));
+        result.append(word2.substring(index, w2Length));
+        return result.toString();
     }
 }
