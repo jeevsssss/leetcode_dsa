9bca9835f044b72fd5381c190d0b9abbf656ca3c
Jeevaa S V <jeevaa.exe@gmail.com>
2023-07-17 22:10:43 +0530

    Time: 30 ms (86.09%), Space: 48.3 MB (63.95%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
+        int[][] result=new int[n+1][m+1];
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                {
+                    result[i][j]=1+result[i-1][j-1];
+                }
+                else
+                {
+                    result[i][j]=Math.max(result[i][j-1],result[i-1][j]);
+                }
+            }
+        }
+        return result[n][m];
+    }
+}
