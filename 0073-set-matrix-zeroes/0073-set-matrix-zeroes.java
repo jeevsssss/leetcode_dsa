345222d7499ab80f3406ceeb170f016da95263b8
Jeevaa S V <jeevaa.exe@gmail.com>
2023-07-20 21:55:07 +0530

    Time: 1 ms (75.47%), Space: 44.6 MB (66.40%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,40 +1,85 @@
 class Solution {
-    public void setZeroes(int[][] matrix){
-
-		int m= matrix.length, n= matrix[0].length;
-        int matrix2[][]= new int[m][n];
-        for(int i=0;i<m;i++)
+    public void setZeroes(int[][] matrix)
+    {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] temp=new int[n][m];
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                matrix2[i][j]=matrix[i][j];
+                temp[i][j]=matrix[i][j];
             }
         }
-        
-        for(int i=0;i<m;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
                     for(int k=0;k<n;k++)
                     {
-                        matrix2[i][k]=0;
+                        temp[k][j]=0;
                     }
                     for(int k=0;k<m;k++)
                     {
-                        matrix2[k][j]=0;
+                        temp[i][k]=0;
                     }
                 }
             }
         }
-    
-        for(int i=0;i<m;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                matrix[i][j]=matrix2[i][j];
+                matrix[i][j]=temp[i][j];
             }
         }
+        
     }
 }
+
+
+
+
+
+
+//   public void setZeroes(int[][] matrix){
+
+// 		int m= matrix.length, n= matrix[0].length;
+//         int matrix2[][]= new int[m][n];
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 matrix2[i][j]=matrix[i][j];
+//             }
+//         }
+        
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(matrix[i][j]==0)
+//                 {
+//                     for(int k=0;k<n;k++)
+//                     {
+//                         matrix2[i][k]=0;
+//                     }
+//                     for(int k=0;k<m;k++)
+//                     {
+//                         matrix2[k][j]=0;
+//                     }
+//                 }
+//             }
+//         }
+    
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 matrix[i][j]=matrix2[i][j];
+//             }
+//         }
+//     }
+// }
