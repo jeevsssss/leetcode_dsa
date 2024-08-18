25e283f9d65fe9a9a96a7bc8127ff8dc06a5c2e9
Jeevaa S V <jeevaa.exe@gmail.com>
2024-08-18 15:22:49 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (55.01%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,10 +1,14 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int max = 0;
+        // int max = 0;
         // Find the maximum candies that one has
-        for (int x : candies)
-        {
-            max = Math.max(max, x);
+        // for (int x : candies)
+        // {
+        //     max = Math.max(max, x);
+        // }
+        int max = candies[0];
+        for (int i = 1; i < candies.length; i++) {
+            max = Math.max(max, candies[i]);
         }
         List<Boolean> result = new ArrayList<>(candies.length);
         // Add the candies they have with the extra candy and see if that makes them have more than the maximum candies someone has; if it does, then it's true, otherwise, it's false
