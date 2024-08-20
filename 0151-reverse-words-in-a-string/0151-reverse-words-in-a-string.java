a53c15be962d02b7e9f7198b9e14befbf684debd
Jeevaa S V <jeevaa.exe@gmail.com>
2024-08-20 22:10:41 +0530

    Time: 8 ms (51.78%), Space: 42.7 MB (88.63%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,28 +1,28 @@
-// class Solution {
-//     public String reverseWords(String s) {
-//         s=s.trim();
-//         String[] wordsArray = s.split("\\s+");
-//         String[] res=new String[wordsArray.length];
-//         int j=0;
-//         for(int i=wordsArray.length-1;i>=0;i--)
-//         {
-//             res[j]=wordsArray[i];
-//             j++;
-//         }
-//         return String.join(" ",res);
-//     }
-// }
 class Solution {
     public String reverseWords(String s) {
-        String[] arr = s.trim().split("\\s+");
-        int i=0,j=arr.length-1;
-        while(i<j) {
-            String t = arr[i];
-            arr[i] = arr[j];
-            arr[j] = t;
-            i++;
-            j--;
+        s=s.trim();
+        String[] wordsArray = s.split("\\s+");
+        String[] res=new String[wordsArray.length];
+        int j=0;
+        for(int i=wordsArray.length-1;i>=0;i--)
+        {
+            res[j]=wordsArray[i];
+            j++;
         }
-        return String.join(" ", arr);
+        return String.join(" ",res);
     }
 }
+// class Solution {
+//     public String reverseWords(String s) {
+//         String[] arr = s.trim().split("\\s+");
+//         int i=0,j=arr.length-1;
+//         while(i<j) {
+//             String t = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = t;
+//             i++;
+//             j--;
+//         }
+//         return String.join(" ", arr);
+//     }
+// }
